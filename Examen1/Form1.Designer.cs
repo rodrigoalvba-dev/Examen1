de175636468cf64ab2b0ea2361d3af6817f980a9
rodrigo <rodrigoalvba@gmail.com>
2026-02-26 15:45:07 -0600

    agregue la logica del prgrama

--- Examen1/Form1.Designer.cs
+++ Examen1/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            buttCAp = new Button();
+            label1 = new Label();
+            labelISR = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            labelSueNet = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(38, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // buttCAp
+            // 
+            buttCAp.Location = new Point(38, 157);
+            buttCAp.Name = "buttCAp";
+            buttCAp.Size = new Size(94, 29);
+            buttCAp.TabIndex = 1;
+            buttCAp.Text = "Capturar";
+            buttCAp.UseVisualStyleBackColor = true;
+            buttCAp.Click += buttCAp_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(157, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Sueldo del trabajador:";
+            // 
+            // labelISR
+            // 
+            labelISR.AutoSize = true;
+            labelISR.Location = new Point(218, 90);
+            labelISR.Name = "labelISR";
+            labelISR.Size = new Size(37, 20);
+            labelISR.TabIndex = 3;
+            labelISR.Text = " ISR:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(285, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 5;
+            label2.Text = "label2";
+            // 
+            // labelSueNet
+            // 
+            labelSueNet.AutoSize = true;
+            labelSueNet.Location = new Point(236, 166);
+            labelSueNet.Name = "labelSueNet";
+            labelSueNet.Size = new Size(92, 20);
+            labelSueNet.TabIndex = 6;
+            labelSueNet.Text = "Sueldo neto:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(450, 236);
+            Controls.Add(labelSueNet);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(labelISR);
+            Controls.Add(label1);
+            Controls.Add(buttCAp);
+            Controls.Add(textBox1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button buttCAp;
+        private Label label1;
+        private Label labelISR;
+        private Label label3;
+        private Label label2;
+        private Label labelSueNet;
     }
 }
--- Examen1/Form1.cs
+++ Examen1/Form1.cs
@@ -1,4 +1,4 @@
-namespace Examen1
+﻿namespace Examen1
 {
     public partial class Form1 : Form
     {
@@ -6,5 +6,52 @@ namespace Examen1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttCAp_Click(object sender, EventArgs e)
+        {//ISR = Cuota fija + (Sueldo − Límite inferior) × Porcentaje
+            double sueldoneto = 0;
+            double ISR = 0;
+            double n = double.Parse(textBox1.Text);
+            try { if (n == null || n <= 0)
+                    MessageBox.Show("Ingrese un número entero positivo.");
+                else
+                {
+                    if (n <= 746.04)
+                        ISR = n - 0.1 * .0192;
+                    else if (n > 746.04 && n <= 6332.05)
+                        ISR = 14.32 + (n - 746.04) * 0.064;
+                    else if (n > 6332.05 && n <= 11128.01)
+                        ISR = 371.83 + (n - 6332.05) * 0.1088;
+                    else if (n > 11128.01 && n <= 12935.83)
+                        ISR = 893.63 + (n - 11128.01) * 0.16;
+                    else if (n > 12935.83 && n <= 15487.71)
+                        ISR = 1318.26 + (n - 12935.83) * 0.1792;
+                    else if (n > 15487.72 && n <= 31263.49)
+                        ISR = 1972.12 + (n - 15487.71) * .2136;
+                    else if (n > 31263.50 && n <= 49233.00)
+                        ISR = 5850.76 + (n - 31263.49) * 0.2352;
+                    else if (n > 49233.00 && n <= 93993.9)
+                        ISR = 10837.02 + (n - 49233.00) * 0.3;
+                    else if (n > 93993.9 && n <= 125325.2)
+                        ISR = 26177.04 + (n - 93993.9) * 0.32;
+                    else if (n > 125325.2 && n <= 375975.00)
+                        ISR = 38243.68 + (n - 125325.2) * 0.34;
+                    else if (n > 375975.00)
+                        ISR = 113443.68 + (n - 375975.00) * 0.35;
+
+                    sueldoneto = n - ISR;
+                    labelISR.Text = "ISR: " + ISR.ToString();
+                    labelSueNet.Text = "Sueldo Neto: " + sueldoneto.ToString();
+                }
+
+            } catch (FormatException)
+            {
+                MessageBox.Show("Ingrese un número válido.");
+            }
+        }
     }
-}
